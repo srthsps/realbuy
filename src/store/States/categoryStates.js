ffb2c3d797c96074f479656d0c351dc083e9c6a1
sarathsps <srthsps22@gmail.com>
2021-10-16 17:56:05 +0530

    ci-cd test

--- src/store/States/categoryStates.js
+++ src/store/States/categoryStates.js
@@ -6,7 +6,7 @@ export const categories = [
         id:2, name: 'Laptops', image_url: "https://m.media-amazon.com/images/I/81J0QOSKU-L._SL1500_.jpg"
     },
     {
-        id:3, name: 'Televisions', image_url: "https://m.media-amazon.com/images/I/81J0QOSKU-L._SL1500_.jpg"
+        id:3, name: 'Television', image_url: "https://m.media-amazon.com/images/I/81J0QOSKU-L._SL1500_.jpg"
     },
     {
         id:4, name: 'Accessories', image_url: "https://m.media-amazon.com/images/I/81J0QOSKU-L._SL1500_.jpg"
